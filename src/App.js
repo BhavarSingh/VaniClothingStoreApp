e487f09cee0c66f729708c595a35f301f48d3a7d
Rait Pravleen Singh <36415182+lllSniperlyfelll@users.noreply.github.com>
2020-05-23 11:08:47 +0530

    Ui change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import './storestyle.css';
+import './material.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+
+
+import Dashboard from './Components/Layouts/Dashboard';
 
 function App() {
   return (
 
     <Router>
-      <div className="App">  
+      <div> 
         <Switch>
-          
+          <Route exact path ="/" component={Dashboard}/>          
         </Switch>
       </div>
     </Router>
---
+++ src/Components/Layouts/Dashboard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import AppNavbar from './AppNavbar';
+import AllProducts from "../Products/AllProducts";
+import {appTitle} from '../Constants';
+
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <AppNavbar/>
+                <div className="">
+                    <div className="jumbotron bg-accent rounded-0 card border-0 rounded-0 text-center">
+                        <div className="row">
+                            <div className="col-sm-6 mx-auto">
+                                <p className="text-white header-font title-font">
+                                    Get the latest fashion and perfect
+                                    sizes for you right here at {appTitle}.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <AllProducts/>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Dashboard;
